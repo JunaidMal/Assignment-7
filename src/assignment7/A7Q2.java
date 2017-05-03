6ef65383e4190530b48ad9280eb6a896a10ae957
JunaidMal <malij6756@googleapps.wrdsb.ca>
2017-05-03 10:14:23 -0400

    Update A7Q2.java

--- src/assignment7/A7Q2.java
+++ src/assignment7/A7Q2.java
@@ -15,27 +15,28 @@ public class A7Q2 {
      */
     public static void main(String[] args) {
 
+        // the examGrade method is used with the grade of %80
         examGrade(80);
     }
 
     public static void examGrade(int grade) {
-
+// grades %80 and above are A
         if (grade >= 80) {
             System.out.println("A");
         }
-
+// grades between %70 and %79 are B
         if (grade >= 70 && grade < 80) {
             System.out.println("B");
         }
-
+// grades between %60 and %69 are C
         if (grade >= 60 && grade < 70) {
             System.out.println("C");
         }
-
+// grades between %50 and %59 are D
         if (grade >= 50 && grade < 60) {
             System.out.println("D");
         }
-
+// grades below %50 are F
         if (grade < 50) {
             System.out.println("F");
         }
